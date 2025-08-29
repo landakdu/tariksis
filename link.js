d7daf080db7abdfd81ea999bafdbc36ac0444e23
landakdu <durilandak429@gmail.com>
2025-08-29 08:53:26 +0800

    Update link.js

--- link.js
+++ link.js
@@ -1,2 +1,5 @@
 function linkqq288() {location.replace("/");}
 function linkqq188() {location.replace("/");}
+function linkqqangpao() {location.replace("/");}
+function link1001win() {location.replace("/");}
+function link88idr() {location.replace("/");}
