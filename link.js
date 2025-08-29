3c96d0b5e1885e3459f69aadee44f70c682092ee
landakdu <durilandak429@gmail.com>
2025-08-29 10:37:07 +0800

    Update link.js

--- link.js
+++ link.js
@@ -1,6 +1,8 @@
 function linkqq288() {location.replace("https://innovamedgroup.us/");}
+function jumpqq288() {window.open("https://innovamedgroup.us/");}
 function linkqq188() {location.replace("/");}
 function linkqqangpao() {location.replace("/");}
 function link1001win() {location.replace("/");}
 function link88idr() {location.replace("/");}
+
 
