3f40fb0c042c477a6be0834e7fa5c5bc22b0a97a
landakdu <durilandak429@gmail.com>
2025-08-29 11:56:32 +0800

    Update link.js

--- link.js
+++ link.js
@@ -1,10 +1,11 @@
 function linkqq288() {location.replace("https://innovamedgroup.us/");}
 function jumpqq288() {window.open("https://innovamedgroup.us/");}
 function linkqq188() {location.replace("https://www.lebanonrenaissance.org/");}
-function jumpqq188() {location.replace("https://www.lebanonrenaissance.org/");}
+function jumpqq188() {window.open("https://www.lebanonrenaissance.org/");}
 function linkqqangpao() {location.replace("/");}
 function link1001win() {location.replace("/");}
 function link88idr() {location.replace("/");}
+
 
 
 
