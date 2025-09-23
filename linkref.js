304972b88e9c71afb066d5149b37a09d9bdbc0ed
landakdu <durilandak429@gmail.com>
2025-09-23 15:24:14 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -3,5 +3,7 @@ function linktokekwin() {location.replace("http://panenmahkota.lol/daftar?ref=ra
 function linkpphoki() {location.replace("https://pphoki8899.xyz/?ref=rakuyaba");}
 function linkaw8() {location.replace("https://www.aw8jos.com/id-id/register?refid=vpwavmyw");}
 function linkhoki177() {location.replace("https://serverhoki177.com/register?ref=rakuyaba");}
+function linknewliga365() {location.replace("https://newliga365ok.xyz/?action=register&rc=N8SY7QO738&ci=11");}
+
 
 
