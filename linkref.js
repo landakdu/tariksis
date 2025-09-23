d54cbceb70371735f2f8cee355bbc97d077f308a
landakdu <durilandak429@gmail.com>
2025-09-23 12:27:56 +0800

    Add files via upload

---
+++ linkref.js
@@ -0,0 +1,3 @@
+function linktokekwin() {location.replace("http://panenmahkota.lol/daftar?ref=rakuyaba");}
+function linkpphoki() {location.replace("/");}
+function linkaw8() {location.replace("https://www.aw8jos.com/id-id/register?refid=vpwavmyw");}
