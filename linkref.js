02f8fe47171c63c1c42c0e5fbd2edd1fa4e52bed
landakdu <durilandak429@gmail.com>
2025-09-24 10:36:09 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -9,6 +9,8 @@ function linkkijangwin() {location.replace("http://spiritkijangwin-1reff.shop/da
 function linkkas138() {location.replace("https://vip.kas138e.site/?ref=rakuyaba");}
 function linkaquaslot() {location.replace("https://aquaslot7.xyz/?ref=bunsin");}
 function linkcoblos4d() {location.replace("https://usdc.coblosmau.xyz/?ref=rakuyaba");}
+function linkkadobet() {location.replace("https://3win.kadobetb.space/?ref=rakuyaba");}
+
 
 
 
