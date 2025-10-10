2414ede5e2788c11c7cfb543ae136f3f7bdf46c9
landakdu <durilandak429@gmail.com>
2025-10-11 04:07:21 +0800

    Update link.js

--- link.js
+++ link.js
@@ -54,28 +54,5 @@ function linkbebek368() {location.replace("https://bebek368c.com/");}
 function linkqqfun77() {location.replace("https://qqfun77pg1.com/");}
 function linkqq999bet() {location.replace("https://qq999bet5hp.com/");}
 function linkqq7997() {location.replace("https://qq7997tg3.com/");}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function linkqqpusat() {location.replace("https://senisastra.com/");}
 
