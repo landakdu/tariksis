421604eb85fb91b1e7c051b84224023953a39112
landakdu <durilandak429@gmail.com>
2025-10-13 21:10:41 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -3,7 +3,7 @@ function linknama() {location.replace("/");}
 function linktokekwin() {location.replace("http://panenmahkota.lol/daftar?ref=rakuyaba");}
 function linkpphoki() {location.replace("https://halohoki177.com/register?ref=rakuyaba");}
 function linkaw8() {location.replace("https://www.aw8jos.com/id-id/register?refid=vpwavmyw");}
-function linkhoki177() {location.replace("https://serverhoki177.com/register?ref=rakuyaba");}
+function linkhoki177() {location.replace("https://halohoki177.com/register?ref=rakuyaba");}
 function linknewliga365() {location.replace("https://newliga365ok.xyz/?action=register&rc=N8SY7QO738&ci=11");}
 function linklotto01() {location.replace("https://masterlotto01.org/referral/BUNSIN");}
 function linkkijangwin() {location.replace("http://spiritkijangwin-1reff.shop/daftar?ref=rakuyaba");}
@@ -13,6 +13,7 @@ function linkcoblos4d() {location.replace("https://usdc.coblosmau.xyz/?ref=rakuy
 function linkkadobet() {location.replace("https://3win.kadobetb.space/?ref=rakuyaba");}
 function linkratu89() {location.replace("https://ratui5.cfd/register?ref=aOog12X");}
 function linkmpo222() {location.replace("https://mpo222highwin.xyz/register/WNR6893G");}
+
 
 
 
