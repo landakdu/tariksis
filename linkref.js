e4f8807c5d2a44315444f84a1800063e1d9ba22e
landakdu <durilandak429@gmail.com>
2025-10-14 04:49:53 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -7,13 +7,14 @@ function linkhoki177() {location.replace("https://halohoki177.com/register?ref=r
 function linknewliga365() {location.replace("https://newliga365ok.xyz/?action=register&rc=N8SY7QO738&ci=11");}
 function linklotto01() {location.replace("https://masterlotto01.org/referral/BUNSIN");}
 function linkkijangwin() {location.replace("http://spiritkijangwin-1reff.shop/daftar?ref=rakuyaba");}
-function linkkas138() {location.replace("https://vip.kasmaju.com/?ref=rakuyaba");}
+function linkkas138() {location.replace("https://tanpavpn.com/situs/linkresmi/");}
 function linkaquaslot() {location.replace("https://aquaslotnet.com/?ref=bunsin");}
 function linkcoblos4d() {location.replace("https://usdc.coblosmau.xyz/?ref=rakuyaba");}
 function linkkadobet() {location.replace("https://win1.kadobetb.xyz/?ref=rakuyaba");}
 function linkratu89() {location.replace("https://ratui5.cfd/register?ref=aOog12X");}
 function linkmpo222() {location.replace("https://wong.mpo222leo.com/register/WNR6893G");}
 function linkqqbos99() {location.replace("https://senisastra.com/?ref=komisi77");}
+
 
 
 
