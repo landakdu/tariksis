91257e22d667a11930965bf492985a57383ee0f3
landakdu <durilandak429@gmail.com>
2025-10-17 22:48:31 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -2,7 +2,7 @@ document.getElementById('yourform').submit();
 function linknama() {location.replace("/");}
 function linktokekwin() {location.replace("http://panenmahkota.lol/daftar?ref=babisexy");}
 function linkpphoki() {location.replace("https://pphoki8899.xyz/?ref=babisexy");}
-function linkaw8() {location.replace("https://www.aw8jos.com/id-id/register?refid=vpwavmyw");}
+function linkaw8() {location.replace("https://www.aw8jos.com/id-id/register?refid=ymbrprab");}
 function linkhoki177() {location.replace("https://halohoki177.com/register?ref=babisexy");}
 function linknewliga365() {location.replace("https://newliga365ok.xyz/?action=register&rc=N8SY7QO738&ci=11");}
 function linklotto01() {location.replace("https://masterlotto01.org/referral/BUNSIN");}
@@ -22,6 +22,7 @@ function linkkas138() {location.replace("https://tanpavpn.com/situs/linkresmi/")
 function linkhokiwin() {location.replace("https://tanpavpn.com/situs/linkresmi/");}
 function linkmpocasino() {location.replace("https://tanpavpn.com/situs/linkresmi/");}
 function linkn77toto() {location.replace("https://tanpavpn.com/situs/linkresmi/");}
+
 
 
 
