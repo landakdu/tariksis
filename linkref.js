1b85498818b8489b5369f1248bff94f77f73473b
landakdu <durilandak429@gmail.com>
2025-10-17 23:44:05 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -5,13 +5,13 @@ function linkpphoki() {location.replace("https://pphoki8899.xyz/?ref=babisexy");
 function linkaw8() {location.replace("https://www.aw8jos.com/id-id/register?refid=ymbrprab");}
 function linkhoki177() {location.replace("https://halohoki177.com/register?ref=babisexy");}
 function linknewliga365() {location.replace("https://newliga365ok.xyz/?action=register&rc=LEOK60YFBD&ci=11");}
-function linklotto01() {location.replace("https://masterlotto01.org/referral/BABISEXY");}
+function linklotto01() {location.replace("https://masterlotto01.org/referral/BABISEXY");}https://wong.mpo222leo.com/register/JK86G0Y4
 function linkkijangwin() {location.replace("http://spiritkijangwin-1reff.shop/daftar?ref=babisexy");}
 function linkaquaslot() {location.replace("https://aquaslotorg.com/?ref=babisexy");}
 function linkcoblos4d() {location.replace("https://usdc.coblosmau.xyz/?ref=babisexy");}
 function linkkadobet() {location.replace("https://win1.kadobetb.xyz/?ref=babisexy");}
 function linkratu89() {location.replace("https://ratui5.cfd/register?ref=aOog12X");}
-function linkmpo222() {location.replace("https://wong.mpo222leo.com/register/WNR6893G");}
+function linkmpo222() {location.replace("https://wong.mpo222leo.com/register/JK86G0Y4");}
 function linkqqbos99() {location.replace("https://senisastra.com/?ref=komisi77");}
 function link303hoki() {location.replace("https://shipstailor.com/register/Y66KPQ1K");}
 function linkjet88bet() {location.replace("https://jet88antri.site/?ref=rakuyaba");}
@@ -22,6 +22,7 @@ function linkkas138() {location.replace("https://tanpavpn.com/situs/linkresmi/")
 function linkhokiwin() {location.replace("https://tanpavpn.com/situs/linkresmi/");}
 function linkmpocasino() {location.replace("https://tanpavpn.com/situs/linkresmi/");}
 function linkn77toto() {location.replace("https://tanpavpn.com/situs/linkresmi/");}
+
 
 
 
