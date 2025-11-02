42dd2d1029b13eb588856f7fd13f50ff9ab5acb3
landakdu <durilandak429@gmail.com>
2025-11-02 12:35:19 +0800

    Add files via upload

---
+++ referral.js
@@ -0,0 +1,19 @@
+document.getElementById('yourform').submit();
+function linknama() {location.replace("/");}
+function linktokekwin() {location.replace("http://panenmahkota.lol/daftar?ref=babisexy");}
+function linkpphoki() {location.replace("https://pphoki8899.xyz/?ref=babisexy");}
+function linkaw8() {location.replace("https://www.aw8jos.com/id-id/register?refid=ymbrprab");}
+function linkhoki177() {location.replace("https://halohoki177.com/register?ref=babisexy");}
+function linknewliga365() {location.replace("https://newliga365ok.xyz/?action=register&rc=LEOK60YFBD&ci=11");}
+function linklotto01() {location.replace("https://masterlotto01.org/referral/BABISEXY");}
+function linkkijangwin() {location.replace("http://spiritkijangwin-1reff.shop/daftar?ref=babisexy");}
+function linkaquaslot() {location.replace("https://aquaslotasli.com/?ref=babisexy");}
+function linkcoblos4d() {location.replace("https://usdc.coblosmau.xyz/?ref=babisexy");}
+function linkkadobet() {location.replace("https://rodakadobet.com/?ref=babisexy");}
+function linkmpo222() {location.replace("https://wong.mpo222leo.com/register/JK86G0Y4");}
+function linkqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function link303hoki() {location.replace("https://shipstailor.com/register/B48RPJDN");}
+function linkjet88bet() {location.replace("https://jet88antri.site/?ref=babisexy");}
+function linkmpo369() {location.replace("https://mpo369bj.com/register/JQPK80KM");}
+function link8bettingan() {location.replace("https://link1.bettingan.vip/");}
+function linkmpo08() {location.replace("https://mpo08dom.xyz/register/Q2RDGN20");}
