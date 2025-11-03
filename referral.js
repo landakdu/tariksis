e1423c38fbd89ff83b9cc83326a2aa95799d55b8
landakdu <durilandak429@gmail.com>
2025-11-03 08:46:19 +0800

    Update referral.js

--- referral.js
+++ referral.js
@@ -7,7 +7,7 @@ function linkhoki177() {location.replace("https://halohoki177.com/register?ref=b
 function linknewliga365() {location.replace("https://newliga365ok.xyz/?action=register&rc=LEOK60YFBD&ci=11");}
 function linklotto01() {location.replace("https://masterlotto01.org/referral/BABISEXY");}
 function linkkijangwin() {location.replace("http://spiritkijangwin-1reff.shop/daftar?ref=babisexy");}
-function linkaquaslot() {location.replace("https://aquaslotasli.com/?ref=babisexy");}
+function linkaquaslot() {location.replace("https://aquaslotcuan.com/?ref=babisexy");}
 function linkcoblos4d() {location.replace("https://usdc.coblosmau.xyz/?ref=babisexy");}
 function linkkadobet() {location.replace("https://rodakadobet.com/?ref=babisexy");}
 function linkmpo222() {location.replace("https://mpo222november.com/register/JK86G0Y4");}
@@ -17,4 +17,5 @@ function linkjet88bet() {location.replace("https://jet88antri.site/?ref=babisexy
 function linkmpo369() {location.replace("https://mpo369bj.com/register/JQPK80KM");}
 function link8bettingan() {location.replace("https://link1.bettingan.vip/");}
 function linkmpo08() {location.replace("https://mpo08dom.xyz/register/Q2RDGN20");}
+
 
