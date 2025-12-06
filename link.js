d73e0b14ae466501df14e2814939c611d6924f39
landakdu <durilandak429@gmail.com>
2025-12-07 05:57:48 +0800

    Update link.js

--- link.js
+++ link.js
@@ -4,18 +4,17 @@ function linkqqangpao() {location.replace("https://qqangpaosnake.comMember/Strea
 function jumpqqangpao() {location.replace("https://qqangpaosnake.com/Member/StreamlinedRegister?R=38B9DA8D");}
 function link88idr() {location.replace("https://88idrbahu.com/");}
 function jump88idr() {location.replace("https://88idrbahu.com/");}
-function linkqqresmi() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function jumpqqresmi() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkqqpusat() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function jumpqqpusat() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkqqgacor() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function jumpqqgacor() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkqqpusat() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkqqcasino() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkdewa86() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkqqslot() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function jumpqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
+function linkqqresmi() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function jumpqqresmi() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function linkqqpusat() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function jumpqqpusat() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function linkqqgacor() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function jumpqqgacor() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function linkqqpusat() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function linkqqcasino() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function linkdewa86() {location.replace("https://semualinktanpavpn.pages.dev/");}
+function linkqqslot() {location.replace("https://semualinktanpavpn.pages.dev/");}
+
 
 
 
