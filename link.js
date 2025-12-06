8c1e9d344622c892a1cb271e286967dc2fb42a30
landakdu <durilandak429@gmail.com>
2025-12-07 05:26:26 +0800

    Update link.js

--- link.js
+++ link.js
@@ -23,28 +23,19 @@ function linkqqgacor() {location.replace("https://www.senisastra.com/Member/Stre
 function jumpqqgacor() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function linkqq828() {location.replace("https://qq828kr1.com/");}
 function jumpqq828() {location.replace("https://qq828kr1.com/");}
-function linkqqvegas() {location.replace("https://qqvegas1ak.com/");}
-function jumpqqvegas() {location.replace("https://qqvegas1ak.com/");}
-function linkqqkingbet() {location.replace("https://qqkingbet95b.com/");}
-function linkqqextra() {location.replace("https://qqextrausb.com/");}
 function linkqqslebew() {location.replace("https://qqslebewc76.com/");}
 function linkqqterang() {location.replace("https://www.qqbiru147.com/");}
 function linkqqhobi() {location.replace("https://qqhobi41t.com/");}
-function linkqq7887() {location.replace("https://qq7887bcp.com/");}
 function linkqqfunwin() {location.replace("https://qqfunwin8vk.com/");}
-function linkqqmegah() {location.replace("https://qqmegah87w.com/");}
 function linkbebek368() {location.replace("https://bebek368c.com/");}
-function linkqqfun77() {location.replace("https://qqfun77pg1.com/");}
 function linkqq999bet() {location.replace("https://qq999bet5hp.com/");}
-function linkqq7997() {location.replace("https://qq7997tg3.com/");}
 function linkqqpusat() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function linkqqcasino() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function linkdewa86() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function linkqqslot() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function linkqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function jumpqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
-function linkqq8889() {location.replace("https://qq8889vgv.com/");}
-function linkqq1x2() {location.replace("https://qq1x2fs9.com/");}
+
 
 
 
