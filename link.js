6ea3cbe704f35910d588bdd42b080e6a2c4863c0
landakdu <durilandak429@gmail.com>
2025-12-21 23:33:50 +0800

    Update link.js

--- link.js
+++ link.js
@@ -4,6 +4,8 @@ function linkqqangpao() {location.replace("https://qqangpaosnake.comMember/Strea
 function jumpqqangpao() {location.replace("https://qqangpaosnake.com/Member/StreamlinedRegister?R=38B9DA8D");}
 function link88idr() {location.replace("https://88idrbahu.com/");}
 function jump88idr() {location.replace("https://88idrbahu.com/");}
+function link1001win() {location.replace("https://1001winsun.com/");}
+function jump1001win() {location.replace("https://1001winsun.com/");}
 function linkqqresmi() {location.replace("https://semualinktanpavpn.pages.dev/");}
 function jumpqqresmi() {location.replace("https://semualinktanpavpn.pages.dev/");}
 function linkqqpusat() {location.replace("https://semualinktanpavpn.pages.dev/");}
@@ -14,6 +16,7 @@ function linkqqpusat() {location.replace("https://semualinktanpavpn.pages.dev/")
 function linkqqcasino() {location.replace("https://semualinktanpavpn.pages.dev/");}
 function linkdewa86() {location.replace("https://semualinktanpavpn.pages.dev/");}
 function linkqqslot() {location.replace("https://semualinktanpavpn.pages.dev/");}
+
 
 
 
