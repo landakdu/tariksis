9e76d582eb1aba5549c3aa94e81cea1fea18fd17
judisuhu1-ux <judisuhu1@gmail.com>
2026-01-30 08:28:02 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -9,7 +9,7 @@ function link() {location.replace("https://www.judisuhu.com/");}
 function link() {location.replace("https://www.judisuhu.com/");}
 function link() {location.replace("https://www.judisuhu.com/");}
 function link() {location.replace("https://www.judisuhu.com/");}
-function link() {location.replace("https://www.judisuhu.com/");}
+function linkgadunslot() {location.replace("https://www.judisuhu.com/");}
 function linksangkarbet() {location.replace("https://www.judisuhu.com/");}
 function linkwukong778() {location.replace("https://www.judisuhu.com/");}
 function linkdewi138() {location.replace("https://www.judisuhu.com/");}
@@ -321,6 +321,7 @@ function linknuhunslot() {location.replace("https://nuhunslott.site/?Ref=8AuFNHk
 function linkvespa69() {location.replace("https://vsp69.tech/register?ref=h3p7Tj7");}
 function linkmultibet88() {location.replace("https://www.judisuhu.com/");}
 function linkoke62() {location.replace("https://www.judisuhu.com/");}
+
 
 
 
