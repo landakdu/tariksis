392af74f927cf0d2a9151b7db0c09165b1c92f36
bunsin0309 <bunsin0309@gmail.com>
2026-02-07 21:18:09 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -1,6 +1,6 @@
 function link() {location.replace("https://www.judisuhu.com/");}
 function link() {location.replace("https://www.judisuhu.com/");}
-function link() {location.replace("https://www.judisuhu.com/");}
+function linkemperor33() {location.replace("https://www.judisuhu.com/");}
 function linkjuarabet168() {location.replace("https://www.judisuhu.com/");}
 function linkmaster303() {location.replace("https://www.judisuhu.com/");}
 function linktokyo88() {location.replace("https://www.judisuhu.com/");}
@@ -333,6 +333,7 @@ function linknuhunslot() {location.replace("https://nuhunslotoke.monster/?Ref=8A
 function linkvespa69() {location.replace("https://vsp69.academy/register?ref=h3p7Tj7");}
 function linkmultibet88() {location.replace("https://www.judisuhu.com/");}
 function linkoke62() {location.replace("https://www.judisuhu.com/");}
+
 
 
 
