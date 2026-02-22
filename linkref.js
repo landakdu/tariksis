a67e3a51f5dd807c5e20c8b68981e6829689a705
bunsin0309 <bunsin0309@gmail.com>
2026-02-22 09:05:08 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -344,7 +344,7 @@ function linkmurnislot() {location.replace("https://www.judisuhu.com/");}
 function linkhokiraja() {location.replace("https://hokirajashinobi.online/daftar?ref=babisexy");}
 function link12naga() {location.replace("https://www.judisuhu.com/");}
 function linkmadu303() {location.replace("https://www.judisuhu.com/");}
-function linkprimatoto() {location.replace("https://log4.aksesprimatoto.com/register?referral_code=babisexy");}
+function linkprimatoto() {location.replace("https://www.judisuhu.com/");}
 function linkpele4d() {location.replace("https://pele4juara.xyz/?ref=babisexy");}
 function linksatelittogel() {location.replace("http://satelit44.tokomajubersama.com/Register.aspx?ref=2728533&aid=22");}
 function linkrans4d() {location.replace("https://rans4d-jago.net/?ref=babisexy");}
@@ -363,6 +363,7 @@ function linknuhunslot() {location.replace("https://nuhunslotoke.sbs/?Ref=8AuFNH
 function linkvespa69() {location.replace("https://vsp69.rocks/register?ref=h3p7Tj7");}
 function linkmultibet88() {location.replace("https://www.judisuhu.com/");}
 function linkoke62() {location.replace("https://www.judisuhu.com/");}
+
 
 
 
