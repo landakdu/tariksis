1b9da04da8f1e03184f43211c96e8a172f984513
judisuhu1-ux <judisuhu1@gmail.com>
2026-02-26 12:41:35 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -179,7 +179,7 @@ function linkmpo333() {location.replace("https://www.judisuhu.com/");}
 function link66kone() {location.replace("https://www.judisuhu.com/");}
 function linkligahoki() {location.replace("https://www.judisuhu.com/");}
 function linkrajampo() {location.replace("https://link03.raj4mpo.com/register/L7NQ26DL");}
-function linkamavi88() {location.replace("https://zuckamavi.website/register/PB2LRN11");}
+function linkamavi88() {location.replace("https://www.judisuhu.com/");}
 function linkbaron4d() {location.replace("https://baron26.site/mobile/register?ref=babisexy");}
 function linkmansion77() {location.replace("https://www.judisuhu.com/");}
 function linkjawara88() {location.replace("https://www.judisuhu.com/");}
@@ -364,6 +364,7 @@ function linknuhunslot() {location.replace("https://nuhunslotoke.store/?Ref=8AuF
 function linkvespa69() {location.replace("https://vsp69.rocks/register?ref=h3p7Tj7");}
 function linkmultibet88() {location.replace("https://www.judisuhu.com/");}
 function linkoke62() {location.replace("https://www.judisuhu.com/");}
+
 
 
 
