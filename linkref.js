209760abac7c08524a7c217628a45e7e3d8937ba
judisuhu1-ux <judisuhu1@gmail.com>
2026-03-05 13:37:25 +0800

    Update linkref.js

--- linkref.js
+++ linkref.js
@@ -146,7 +146,7 @@ function linkaquaslot() {location.replace("https://aquaslot11.xyz/?ref=babisexy"
 function linkcoblos4d() {location.replace("https://a25.coblos4dbet.site/?ref=babisexy");}
 function linkkadobet() {location.replace("https://www.judisuhu.com/");}
 function linkmpo222() {location.replace("https://mpo222xids.it.com/register/JK86G0Y4");}
-function linkqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=");}
+function linkqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function link303hoki() {location.replace("https://eworldexternal.com/register/B48RPJDN");}
 function linkjet88bet() {location.replace("https://www.judisuhu.com/");}
 function linkmpo369() {location.replace("https://www.judisuhu.com/");}
@@ -364,6 +364,7 @@ function linknuhunslot() {location.replace("https://scatters.shop/bejKhi");}
 function linkvespa69() {location.replace("https://vsp69.rocks/register?ref=h3p7Tj7");}
 function linkmultibet88() {location.replace("https://www.judisuhu.com/");}
 function linkoke62() {location.replace("https://www.judisuhu.com/");}
+
 
 
 
