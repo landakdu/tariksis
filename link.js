41d7560212c6a23aec2f6daee92cd57c00e420bc
judisuhu1-ux <judisuhu1@gmail.com>
2026-03-13 20:31:04 +0700

    Update link.js

--- link.js
+++ link.js
@@ -1,7 +1,7 @@
 function linkqqbonus() {location.replace("/");}
 function jumpqqbonus() {location.replace("/");}
-function linkqqangpao() {location.replace("/");}
-function jumpqqangpao() {location.replace("/");}
+function linkqqangpao() {location.replace("https://qqangpaosnake.com/Member/StreamlinedRegister?R=38B9DA8D");}
+function jumpqqangpao() {location.replace("https://qqangpaosnake.com/Member/StreamlinedRegister?R=38B9DA8D");}
 function link88idr() {location.replace("/");}
 function jump88idr() {location.replace("/");}
 function link1001win() {location.replace("/");}
@@ -20,6 +20,10 @@ function linkqq101() {location.replace("/");}
 function jumpqq101() {location.replace("/");}
 function linkqq188() {location.replace("https://qq188best.com/");}
 function jumpqq188() {location.replace("https://qq188best.com/");}
+function linkqq288() {location.replace("https://qq288im3.com/");}
+function jumpqq288() {location.replace("https://qq288im3.com/");}
+
+
 
 
 
